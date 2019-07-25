4db530909d22c9321a00c249be41efdbda2090e4
Florian Krohmer <florian.krohmer@gmail.com>
2019-07-25 13:27:47 +0200

    Reduce to one button in the Backend for update and import. Add auto fill for author field.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,10 +5,5 @@
  */
 $GLOBALS['BE_MOD']['content']['opasImport'] = array
 (
-    'callback' => 'Floxn\ContaoOpasImportBundle\Module\OpasImportModule'
-);
-
-$GLOBALS['BE_MOD']['content']['opasRefresh'] = array
-(
     'callback' => 'Floxn\ContaoOpasImportBundle\Module\OpasRefreshModule'
 );
