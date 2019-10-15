7aa3ef9f2c4e8df911fedc054aaffa77bcc8e6dc
Florian Krohmer <florian.krohmer@gmail.com>
2019-10-15 11:54:00 +0200

    change description

--- src/Resources/contao/dca/tl_calendar_events.php
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -3,13 +3,13 @@
 // Erweitern der default Feldposition mit zwei weiteren: konzertID und konzertOrt
 $GLOBALS['TL_DCA']['tl_calendar_events']['palettes']['default'] = str_replace(
     'location',
-    'location, konzertID, konzertOrt',
+    'location, concertID, concertOrt, ',
     $GLOBALS['TL_DCA']['tl_calendar_events']['palettes']['default']);
 
 // Feldkonfiguration für konzertID
-$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['konzertID'] = array
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['concertID'] = array
 (
-'label'		=> &$GLOBALS['TL_LANG']['tl_calendar_events']['konzertID'],
+'label'		=> &$GLOBALS['TL_LANG']['tl_calendar_events']['concertID'],
 'exclude'	=> true,
 'search'	=> true,
 'inputType'	=> 'text',
@@ -18,9 +18,9 @@ $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['konzertID'] = array
 );
 
 // Feldkonfiguration für konzertOrt
-$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['konzertOrt'] = array
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['concertOrt'] = array
 (
-'label'		=> &$GLOBALS['TL_LANG']['tl_calendar_events']['konzertOrt'],
+'label'		=> &$GLOBALS['TL_LANG']['tl_calendar_events']['concertOrt'],
 'exclude'	=> true,
 'search'	=> true,
 'inputType'	=> 'text',
