d2850cd5c302407435dc561c779cbb2cf9648db2
Florian Krohmer <florian.krohmer@gmail.com>
2019-10-15 10:51:02 +0200

    change description in the Backend in canlendar_events.

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,5 +1,5 @@
 $GLOBALS['TL_LANG']['tl_calendar_events']['konzertID']['0']  = "Konzert ID oder Ausverkauft";
-$GLOBALS['TL_LANG']['tl_calendar_events']['konzertID']['1']  = "Bitte hier die Konzert ID eintragen. Bei ausverkaufter Veranstaltung bitte '1' eintragen. Ist keine Kartenverkauf möglich bitte '0' eintragen.";
+$GLOBALS['TL_LANG']['tl_calendar_events']['konzertID']['1']  = "Bitte tragen Sie die Konzert ID ein. Bei ausverkaufter Veranstaltung bitte '1' eintragen. Ist keine Kartenverkauf möglich bitte '0' eintragen.";
 
 $GLOBALS['TL_LANG']['tl_calendar_events']['konzertOrt']['0']  = "Konzert Ort";
 $GLOBALS['TL_LANG']['tl_calendar_events']['konzertOrt']['1']  = "Bitte hier den Konzertort eintragen";
