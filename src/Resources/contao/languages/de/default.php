2cf82628a7459ebdc92a53b193721db0ff33e34e
Florian Krohmer <florian.krohmer@gmail.com>
2019-10-15 11:00:50 +0200

    add <?php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,3 +1,5 @@
+<?php
+
 $GLOBALS['TL_LANG']['tl_calendar_events']['konzertID']['0']  = "Konzert ID oder Ausverkauft";
 $GLOBALS['TL_LANG']['tl_calendar_events']['konzertID']['1']  = "Bitte tragen Sie die Konzert ID ein. Bei ausverkaufter Veranstaltung bitte '1' eintragen. Ist keine Kartenverkauf möglich bitte '0' eintragen.";
 
